89c8dc5a0bbcb12254570e6af80b73491bdc0606
pham-van-man <phamvanman.com.vn@gmail.com>
2024-11-12 08:46:12 +0700

    first commit

--- src/components/uncommon/EditStaff.js
+++ src/components/uncommon/EditStaff.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import {toast} from "react-toastify";
 import axios from "axios";
 
-
 function EditStaff() {
 
     const [selectedEmployee, setSelectedEmployee] = useState(null);
@@ -62,109 +61,107 @@ function EditStaff() {
     };
 
     return (
-        <div>
-            {selectedEmployee && (
-                <div className="modal fade show" id="updateModal" style={{display: 'block'}} aria-hidden="true"
-                     tabIndex="-1">
-                    <div className="modal-dialog modal-lg">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Cập nhật thông tin nhân viên</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"
-                                        onClick={() => setSelectedEmployee(null)}></button>
+        selectedEmployee && (
+            <div className="modal fade show" id="updateModal" style={{display: 'block'}} aria-hidden="true"
+                 tabIndex="-1">
+                <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Cập nhật thông tin nhân viên</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                    onClick={() => setSelectedEmployee(null)}></button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Họ tên</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="name"
+                                    name="name"
+                                    value={updatedEmployee.name}
+                                    onChange={handleInputChange}
+                                />
                             </div>
-                            <div className="modal-body">
-                                <div className="mb-3">
-                                    <label htmlFor="name" className="form-label">Họ tên</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="name"
-                                        name="name"
-                                        value={updatedEmployee.name}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="gender" className="form-label">Giới tính</label>
-                                    <select
-                                        className="form-select"
-                                        id="gender"
-                                        name="gender"
-                                        value={updatedEmployee.gender}
-                                        onChange={handleInputChange}
-                                    >
-                                        <option value={true}>Nam</option>
-                                        <option value={false}>Nữ</option>
-                                    </select>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="address" className="form-label">Địa chỉ</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="address"
-                                        name="address"
-                                        value={updatedEmployee.address}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="phone" className="form-label">Số điện thoại</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="phone"
-                                        name="phone"
-                                        value={updatedEmployee.phone}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="email" className="form-label">Email</label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        id="email"
-                                        name="email"
-                                        value={updatedEmployee.email}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="salary" className="form-label">Lương</label>
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        id="salary"
-                                        name="salary"
-                                        value={updatedEmployee.salary}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="startDate" className="form-label">Ngày bắt đầu</label>
-                                    <input
-                                        type="date"
-                                        className="form-control"
-                                        id="startDate"
-                                        name="startDate"
-                                        value={updatedEmployee.startDate}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
+                            <div className="mb-3">
+                                <label htmlFor="gender" className="form-label">Giới tính</label>
+                                <select
+                                    className="form-select"
+                                    id="gender"
+                                    name="gender"
+                                    value={updatedEmployee.gender}
+                                    onChange={handleInputChange}
+                                >
+                                    <option value={true}>Nam</option>
+                                    <option value={false}>Nữ</option>
+                                </select>
                             </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"
-                                        onClick={() => setSelectedEmployee(null)}>Đóng
-                                </button>
-                                <button type="button" className="btn btn-success" onClick={handleUpdate}>Cập nhật
-                                </button>
+                            <div className="mb-3">
+                                <label htmlFor="address" className="form-label">Địa chỉ</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="address"
+                                    name="address"
+                                    value={updatedEmployee.address}
+                                    onChange={handleInputChange}
+                                />
                             </div>
+                            <div className="mb-3">
+                                <label htmlFor="phone" className="form-label">Số điện thoại</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="phone"
+                                    name="phone"
+                                    value={updatedEmployee.phone}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    name="email"
+                                    value={updatedEmployee.email}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="salary" className="form-label">Lương</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    id="salary"
+                                    name="salary"
+                                    value={updatedEmployee.salary}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="startDate" className="form-label">Ngày bắt đầu</label>
+                                <input
+                                    type="date"
+                                    className="form-control"
+                                    id="startDate"
+                                    name="startDate"
+                                    value={updatedEmployee.startDate}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"
+                                    onClick={() => setSelectedEmployee(null)}>Đóng
+                            </button>
+                            <button type="button" className="btn btn-success" onClick={handleUpdate}>Cập nhật
+                            </button>
                         </div>
                     </div>
                 </div>
-                </div>
-                )
-            }
+            </div>)
+    )
+}
